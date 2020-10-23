21d9bc29efa8c9dc3b5245ec258b7348fba43a55
darrenfok <66759042+darrenfok@users.noreply.github.com>
2020-10-23 02:00:41 -0700

    Lab02 both versions

---
+++ Lab02/Lab02v100/Lab02v100.java
@@ -0,0 +1,29 @@
+// Lab02v100.java
+// This is the 100 point version.
+// Darren Fok
+// October 11th, 2020
+// APCS, Mr. Robinson
+// The 100-point version of Lab 02 is an excercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file. 
+
+public class Lab02v100
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Lab 02, 100 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure today's knowledge ");
+      System.out.println( "by yesterday's confusion.");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("# ##### #####     ##### ##### # #   # ##### #####");
+      System.out.println("# #   # #   #     #   # #   # # ##  #   #   #    ");
+      System.out.println("# #   # #   #     ##### #   # # # # #   #   #####");
+      System.out.println("# #   # #   #     #     #   # # #  ##   #       #");
+      System.out.println("# ##### #####     #     ##### # #   #   #   #####");
+   }
+}
+      
---
+++ Lab02/Lab02v80/Lab02v80.java
@@ -0,0 +1,24 @@
+// Lab02v80.java
+// This is the 80 point version.
+// Darren Fok
+// October 11th, 2020
+// APCS, Mr. Robinson
+// The 80-point version of Lab 02 is an excercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file. 
+
+public class Lab02v80
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Lab 02, 80 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure today's knowledge ");
+      System.out.println( "by yesterday's confusion.");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+   }
+}
+      
